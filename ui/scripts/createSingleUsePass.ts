e837d3df580068b0c43837b906aabf19688d520c
dankelleher <dankelleher@yahoo.com>
2022-06-25 16:22:56 +0200

    v0.0.3

---
+++ ui/scripts/createSingleUsePass.ts
@@ -0,0 +1,19 @@
+import { DerivedPassService } from "../../dist/src";
+import {AnchorProvider, web3} from "@project-serum/anchor";
+
+// Civic Uniqueness Pass
+const constituentPass = new web3.PublicKey("uniqobk8oGh4XBLMqM68K8M2zNu3CdYX7q5go7whQiv");
+
+(async () => {
+  const provider = AnchorProvider.env();
+  const service = await DerivedPassService.build(provider);
+
+  const [txSignature, derivedPass] = await service.derivePass([
+    constituentPass
+  ], {
+    expireOnUse: true,
+    refreshDisabled: true,
+  });
+
+  console.log(derivedPass.toBase58());
+})().catch(console.error);
